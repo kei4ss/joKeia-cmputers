ed9429995f468becd554fd2d53b85a9a7320a9ce
Miquéias Ferreira Dos Santos <queias2909@gmail.com>
2026-02-22 00:51:14 -0300

    Backend: first commit

---
+++ backend/src/main/java/com/cmputers/jokeia/demo/JokeiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cmputers.jokeia.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeiaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cmputers/jokeia/demo/JokeiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmputers.jokeia.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeiaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
